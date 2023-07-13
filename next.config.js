a4ee2b1d1335d51ed612fc0130165ea4a23dc2ba
MarioPayan <mpayan.av@gmail.com>
2023-07-12 22:14:18 -0500

    Solving minor issues

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
-    localeDetection: false,
-  },
+  // i18n: { TODO: enable when ready
+  //   locales: ['en', 'es'],
+  //   defaultLocale: 'en',
+  //   localeDetection: false,
+  // },
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/src/api/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
